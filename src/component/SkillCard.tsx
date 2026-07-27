1199cf3f8e97d7f0014832bed3d772211b199e5c
fguirama <florian.guiramand@protonmail.com>
2026-07-27 16:36:10 +0200

    fix: pourcent skill display

--- src/component/SkillCard.tsx
+++ src/component/SkillCard.tsx
@@ -2,17 +2,18 @@ import {View,Text} from "react-native";
 import {iSkill} from "@/types/type.42";
 
 export default function SkillCard({item}: {item: iSkill}){
-    const level = Math.round(item.level);
+    const intLevel = Math.floor(item.level);
+    const pourcentLevel = Math.round((item.level % 1) * 100);
 
     return (<View className="bg-gray-300 rounded-2xl p-4 mb-4">
         <View className="flex-row justify-between">
             <Text className="text-gray-900">{item.name}</Text>
-            <Text className="text-gray-900 font-bold">{level}</Text>
+            <Text className="text-gray-900 font-bold">{intLevel}</Text>
         </View>
         <View className="bg-gray-500 h-3 rounded-full mt-3">
             <View
                 style={{
-                    width:`${level * 4}%`
+                    width:`${pourcentLevel}%`
                 }}
                 className="bg-gray-900 h-3 rounded-full"/>
         </View>
